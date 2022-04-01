6ce06d924f8dfb908d8593f12b6e221d74b468c8
Govind Verma <100123540+GovindVermagkv@users.noreply.github.com>
2022-04-01 23:54:01 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,70 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="common.css">
+    <!-- <link rel="stylesheet" href="css/bootstrap.min.css"> -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.11.2/css/all.css">
+</head>
+
+<body>
+
+    <div class="container">
+        <div class="main">
+            <div class="icon">
+                <img src="img/logo.png">
+            </div>
+
+            <div class="navbar">
+                <img src="img/user-solid.svg" alt="">
+                <a href="ragister_form2.php"> signup</a>
+                <h3>|</h3>
+                <img src="img/right-to-bracket-solid.svg" alt="">
+                <a href="login_form2.php">login</a>
+            </div>
+        </div>
+
+
+        <div class="search">
+            <h1>Happines Per Square Root</h1>
+
+            <div class="search-icon">
+                <input type="text" placeholder="Enter Your City To search for PGs">
+                <i class="fas fa-search"></i>
+            </div>
+
+        </div>
+        <div class="logo">
+            <h1>major-cities</h1>
+            <div class="city-logo">
+               <a href="property_list.php"><img src="img/delhi.png" alt="" srcset=""></a>
+               <a href="property_list.php"> <img src="img/bangalore.png" alt=""></a>
+               <a href="property_list.php">  <img src="img/mumbai.png" alt=""></a>
+               <a href="property_list.php">  <img src="img/hyderabad.png" alt=""></a>
+            </div>
+        </div>
+
+        <div class="footer">
+            <div class="visit">
+               <a href="#"> <h3>PG in Delhi</h3></a>
+               <a href="#"><h3>PG in mumbai</h3></a>
+               <a href="#"><h3>PG in bangalore</h3></a>
+               <a href="#"><h3>PG in hyderabad</h3></a>
+            </div>
+            <div class="end">
+            <p class="copyright">A&copy; 20xx copyright PG life</p>
+            </div>
+        </div>
+
+
+
+
+
+    </div>
+</body>
+
+</html>
